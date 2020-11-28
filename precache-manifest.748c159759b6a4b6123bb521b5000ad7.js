5fc613696bd204651df01cb86b06e9a2860687c8
Sam Bevans-Kerr <sambk@macbook-pro.attlocal.net>
2020-11-28 14:11:01 -0500

    Updates

---
+++ precache-manifest.748c159759b6a4b6123bb521b5000ad7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff3d9bc82f1479863b9e0a5dee10f70",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "44be63fd3a5cc5f59a31",
+    "url": "/react-portfolio/static/css/main.085804d4.chunk.css"
+  },
+  {
+    "revision": "1b213dae0efc96870df6",
+    "url": "/react-portfolio/static/js/2.a080821b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.a080821b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44be63fd3a5cc5f59a31",
+    "url": "/react-portfolio/static/js/main.247c7c7b.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "c5d6b02e5002416a0af0a8c76d415395",
+    "url": "/react-portfolio/static/media/amber.c5d6b02e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
